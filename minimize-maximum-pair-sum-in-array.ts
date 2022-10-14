35d959ea77ef667b628ad395cbe834e9f4426703
nmktad <nmktadesse@gmail.com>
2022-10-14 12:23:57 +0300

    the fastest submission

---
+++ minimize-maximum-pair-sum-in-array.ts
@@ -0,0 +1,11 @@
+function minPairSum(nums: number[]): number {
+  nums.sort((i, j) => i - j);
+
+  let max = 0;
+
+  for (let i = 0; i < nums.length / 2; i++) {
+    max = Math.max(max, nums[i] + nums[nums.length - 1 - i]);
+  }
+
+  return max;
+}
